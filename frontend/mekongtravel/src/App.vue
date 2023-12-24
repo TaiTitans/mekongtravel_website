d0fed98617a21e2cda10d88f5b693e9efe5e966c
Phan Phat Tai <70010376+TaiTitans@users.noreply.github.com>
2023-12-24 23:51:24 +0700

    update login

--- frontend/mekongtravel/src/App.vue
+++ frontend/mekongtravel/src/App.vue
@@ -1,14 +1,21 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 </script>
-
+<style>
+.body_login{
+    height: 100vh; width: 100vw; 
+}
+.form_login{
+    height:800px;
+}
+</style>
 <template>
-  <body>
-    <section class="bg-gray-100 min-h-screen flex box-border justify-center items-center">
-    <div class="bg-[#dfa674] rounded-2xl flex max-w-3xl p-5 items-center">
+  <body class='body_login'>
+    <section class="bg-gray-100 h-full flex box-border justify-center items-center">
+    <div class="bg-white shadow-xl rounded-2xl flex w-9/12 p-5 items-center form_login">
         <div class="md:w-1/2 px-8">
-            <h2 class="font-bold text-3xl text-[#002D74]">Login</h2>
-            <p class="text-sm mt-4 text-[#002D74]">If you already a member, easily log in now.</p>
+            <h2 class="font-bold text-3xl text-[#002D74]">Đăng nhập</h2>
+            <p class="text-sm mt-4 text-[#002D74]">Nếu bạn đã có tài khoản, hãy đăng nhập.</p>
 
             <form action="" class="flex flex-col gap-4">
                 <input class="p-2 mt-8 rounded-xl border" type="email" name="email" placeholder="Email">
@@ -35,14 +42,14 @@ import { RouterLink, RouterView } from 'vue-router'
                         </path>
                     </svg>
                 </div>
-                <button class="bg-[#002D74] text-white py-2 rounded-xl hover:scale-105 duration-300 hover:bg-[#206ab1] font-medium" type="submit">Login</button>
+                <button class="bg-[#002D74] text-white py-2 rounded-xl hover:scale-105 duration-300 hover:bg-[#206ab1] font-medium" type="submit">Đăng nhập</button>
             </form>
-            <div class="mt-10 grid  items-center text-gray-500 or-grid">
-                <hr class="border-gray-400">
-                <p class="text-center text-sm">OR</p>
-                <hr class="border-gray-400">
+            <div class="mt-10 grid  items-center text-gray-400 or-grid">
+                <hr class="border-gray-300">
+                <p class="text-center text-sm">hoặc</p>
+                <hr class="border-gray-300">
             </div>
-            <button class="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 hover:bg-[#60a8bc4f] font-medium">
+            <button class="text-black bg-white border py-2 w-1/2 rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 hover:bg-[#60a8bc4f] font-medium">
                     <svg class="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
                         <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path>
                         <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path>
@@ -50,17 +57,23 @@ import { RouterLink, RouterView } from 'vue-router'
                         <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
                     </svg>
 
-                    Login with Google
+                    Đăng nhập bằng Google
                 </button>
-            <div class="mt-10 text-sm border-b border-gray-500 py-5 playfair tooltip">Forget password?</div>
+
+
+            <div class="mt-10 border-b border-gray-500 py-5">
+                <a class='text-[#325285] underline'>
+                    Quên mật khẩu? 
+                </a>
+            </div>
 
             <div class="mt-4 text-sm flex justify-between items-center container-mr">
-                <p class="mr-3 md:mr-0 ">If you don't have an account..</p>
-                <button class="hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300">Register</button>
+                <a class="mr-3 md:mr-0 text-[#325285]">Chưa có tài khoản ?</a>
+                <button class="register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300">Đăng ký</button>
             </div>
         </div>
         <div class="md:block hidden w-1/2">
-            <img class="rounded-2xl max-h-[1600px]" src="https://loremflickr.com/cache/resized/65535_52650182739_9349929720_h_640_1280_nofilter.jpg" alt="login form image">
+            <img class="rounded-2xl max-h-[1600px]" src="./assets/img/background_login.png" alt="login form image">
         </div>
     </div>
 </section>
--- frontend/mekongtravel/src/router/index.js
+++ frontend/mekongtravel/src/router/index.js
@@ -1,4 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+    {
+        path:"/",
+        name:'login',
+        component: () => import('../App.vue')
+    },
+    {
+        path:'/register',
+        name: 'Register',
+        component: () => import('../views/register.vue')
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory('/'),
+    routes
+})
 
 export default router
